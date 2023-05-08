37e5d79e092587df203a514fe480031f4d65c1e9
giuratofabrizio <giurato.fabrizio@iisgalvanimi.edu.it>
2023-05-08 08:59:32 +0000

    fine

--- googleMapsShapes/src/app/app.component.ts
+++ googleMapsShapes/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements AfterViewInit {
   geoJsonObject : GeoFeatureCollection;
   fillColor: string = "#FF0000";  //Colore delle zone catastali
  
-  markerList : google.maps.MarkerOptions[] | undefined;
+  markerList!: google.maps.MarkerOptions[];
   zoom = 8;
 
   constructor() {
@@ -23,23 +23,44 @@ export class AppComponent implements AfterViewInit {
     //Questi dati dovremmo scaricarli dal server, per ora li abbiamo copiati nel file     gojson.model.ts
     this.geoJsonObject = GEOJSON;  
     console.log(this.geoJsonObject); //stampo l'oggetto geoJsonObject sulla console
+    this.markerGenerator()
   }
 
   @ViewChild('mapRef') mapRef!: GoogleMap;
   ngAfterViewInit() {
     this.mapRef.data.addGeoJson(this.geoJsonObject);
-    this.mapRef.data.setStyle(this.styleFunc)
+    this.mapRef.data.setStyle(this.styleFunc);
   }
 
   styleFunc = (feature:any) =>{
-    console.log(feature.i.id)
-    let newColor = "#FF0000"; //RED
-    if(feature.i.id == 0) newColor = "#00FF00"; //GREEN
+    console.log(feature)
+    let newColor = "#800080"; //RED
+    if(feature.h.id == 0) newColor = "#00FF00"; //GREEN
     else newColor = "#0000FF"; //BLUE
     return ({
       clickable: false,
       fillColor: newColor,
       strokeWeight: 1
     });
+  }
+  markerGenerator()
+  {
+    this.markerList =[
+      {
+        position : {
+          lng : this.geoJsonObject.features[0].geometry.coordinates[0][0][0],
+          lat : this.geoJsonObject.features[0].geometry.coordinates[0][0][1]
+        },
+        label: String(this.geoJsonObject.features[0].properties.id),
+      },
+      {
+        position : {
+          lng : this.geoJsonObject.features[1].geometry.coordinates[0][0][0],
+          lat : this.geoJsonObject.features[1].geometry.coordinates[0][0][1]
+        },
+        label: String(this.geoJsonObject.features[1].properties.id),
+      },
+    ]
+
   }
 }
